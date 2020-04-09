b3cf3ac2f03421364ebb53c53152b67639c40261
nicktfranklin <nthompsonsonfranklin@gmail.com>
2020-04-09 12:21:57 -0400

    better simulation queue handeling

--- schema_prediction_batch_runner_04-06-20.py
+++ schema_prediction_batch_runner_04-06-20.py
@@ -57,7 +57,42 @@ f_opts=dict(
     optimizer_kwargs=optimizer_kwargs)
 
 
-# def run_single_batch(batch_number, lr, n_epochs, log_alpha, log_lambda, mixed=False):
+
+def make_random_queue(batch_n, lrs, n_epochs_, log_alphas, log_lambdas, mixed=False):
+    # go through these lists in a random order for better sampling
+    parameter_tuples = []
+    for lr in lrs:
+        for n_epochs in n_epochs_:
+            for log_alpha in log_alphas:
+                for log_lambda in log_lambdas:
+                    parameter_tuples.append((lr, n_epochs, log_alpha, log_lambda))
+
+    n = len(parameter_tuples)  # total number of simulations to be run
+
+    # loop through the parameters in a random order, look for corresponding files,
+    # and if none are found, add to the cue.
+    parameters_queue = []
+    for lr, n_epochs, log_alpha, log_lambda in np.random.permutation(parameter_tuples):
+        
+        # # list of files that specify the simulation experiment exactly
+        # args = [epsilon, lr, int(n_epochs), dropout, log_alpha, log_lambda, int(batch_n)]
+        # files = glob('./json_files/*{}*{}*{}*{}*{}*{}*{}*'.format(*args))
+
+        args = [epsilon, lr, int(n_epochs), dropout, log_alpha, log_lambda, int(batch_n)]
+        file_string = './json_files/results*{}*{}*{}*{}*{}*{}*{}*'.format(*args)
+        
+        if mixed:
+            file_string += 'mixed*'
+
+        files = glob(file_string)
+
+        if not files:
+            parameters_queue.append((lr, int(n_epochs), log_alpha, log_lambda))
+
+    t = len(parameters_queue)
+    print('Found {} of {} simulations previous completed'.format(n-t, n))
+    return parameters_queue
+
 
 def run_single_batch(batch_n, lr, n_epochs, log_alpha, log_lambda, mixed=False):
     # sem prior params (dosen't rely on mutability)
@@ -70,51 +105,50 @@ def run_single_batch(batch_n, lr, n_epochs, log_alpha, log_lambda, mixed=False):
         epsilon, lr, n_epochs, dropout, log_alpha, log_lambda, batch_n)
     json_tag_mixed = '_e{}_lr{}_n{}_d{}_logalfa_{}_loglmda_{}_batch_{}_mixed'.format(
         epsilon, lr, n_epochs, dropout, log_alpha, log_lambda, batch_n)
-    print(json_tag)
 
     if not mixed:
-        # Don't want to re-run things...
-        # check for the file existance of this simulation
-        files = glob('./json_files/*{}*'.format(json_tag))
-        files += glob('./*{}*'.format(json_tag))
-        if not files:
-            _, _, _ = batch_exp(
-                sem_kwargs, story_kwargs, n_batch=1, sem_progress_bar=True,
-                progress_bar=False, block_only=False,
-                run_mixed=False,
-                save_to_json=True,
-                json_tag=json_tag,
-                json_file_path='./json_files/')
+        _, _, _ = batch_exp(
+            sem_kwargs, story_kwargs, n_batch=1, sem_progress_bar=True,
+            progress_bar=False, block_only=False,
+            run_mixed=False,
+            save_to_json=True,
+            json_tag=json_tag,
+            json_file_path='./json_files/')
     else:
-        files = glob('./json_files/*{}*'.format(json_tag_mixed))
-        files += glob('./*{}*'.format(json_tag_mixed))
-        if not files:
-            _, _, _ = batch_exp(
-                sem_kwargs, story_kwargs, n_batch=1, sem_progress_bar=True,
-                progress_bar=False, block_only=False,
-                run_mixed=True,
-                save_to_json=True,
-                json_tag=json_tag_mixed,
-                json_file_path='./json_files/')
+        _, _, _ = batch_exp(
+            sem_kwargs, story_kwargs, n_batch=1, sem_progress_bar=True,
+            progress_bar=False, block_only=False,
+            run_mixed=True,
+            save_to_json=True,
+            json_tag=json_tag_mixed,
+            json_file_path='./json_files/')
 
-lrs = [0.00025, 0.0005, 0.001, 0.002, 0.004, 0.008, 0.01]
-n_epochs_ = [20, 24, 28, 32]
-log_alphas = [0.0, -5.0, -2.5,  2.5, 5.0, 7.5, -7.5]
-log_lambdas = [0.0, 2.5, 5.0, 7.5, 10.0,]
+# lrs = [0.0005, 0.001, 0.002, 0.003, 0.004, 0.008]
+# n_epochs_ = [12, 16, 20, 24, 28, 32, 36, 40, 44]
+# log_alphas = [0.0, -5.0, -2.5,  2.5, 5.0, 7.5, -7.5, -10.0, -15.0]
+# log_lambdas = [0.0, 2.5, 5.0, 7.5, 10.0, 15.0, 20.0, 30.0, 40.0]
 
-t = 0
+lrs = [ 0.001, 0.002]
+n_epochs_ = [32, 36, 40, 44]
+log_alphas = [0.0, -7.5, -10.0, -15.0, -20.0, -25.0]
+log_lambdas = [0.0, 10.0, 15.0, 20.0, 30.0, 40.0]
+
 
 if __name__ == "__main__":
+    import sys
+    try:
+        batch_n = int(sys.argv[1])
+        "Running Batch {}:".format(batch_n)
+    except:
+        print('Please specify batch number!')
+        raise(Exception)
 
-    batch_n = 7
+    mixed=False
 
-    # go through these lists in a random order for better sampling
-    parameter_tuples = []
-    for lr in lrs:
-        for n_epochs in n_epochs_:
-            for log_alpha in log_alphas:
-                for log_lambda in log_lambdas:
-                    parameter_tuples.append((lr, n_epochs, log_alpha, log_lambda))
-
-    for lr, n_epochs, log_alpha, log_lambda in np.random.permutation(parameter_tuples):
-        run_single_batch(batch_n, lr, int(n_epochs), log_alpha, log_lambda, mixed=False)
+    parameters_queue = make_random_queue(batch_n, lrs, n_epochs_, log_alphas, log_lambdas, mixed=mixed)
+    t = 0
+    n = len(parameters_queue)
+    for lr, n_epochs, log_alpha, log_lambda in np.random.permutation(parameters_queue):
+        print("Running simulation {} of {}".format(t, n))
+        run_single_batch(batch_n, lr, int(n_epochs), log_alpha, log_lambda, mixed=mixed)
+        t += 1

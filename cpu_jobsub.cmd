48e74f77c928a37d6614c4b2441f7d4a5c857d8a
andrebeu <aobeukers@gmail.com>
2021-01-21 11:08:48 -0500

    loop jobsub ready

--- cpu_jobsub.cmd
+++ cpu_jobsub.cmd
@@ -1,22 +1,23 @@
-#!/bin/bash
-
+#SBATCH --mail-type=begin
+#SBATCH --mail-type=end
 #SBATCH --mail-user=abeukers@princeton.edu
 
-#SBATCH -t 04:00:00			# runs for 48 hours (max)  
+#SBATCH -t 6:00:00   # runs for 48 hours (max)  
 #SBATCH --ntasks-per-node=1
-#SBATCH -N 1				# node count 
-#SBATCH -c 16				# number of cores 
+#SBATCH -N 1         # node count 
+#SBATCH -c 4         # number of cores 
+#SBATCH -o ./slurms/output.%j.%a.out
 
 
 module load pyger/0.9
-conda activate sem
+source activate sem
 
 lr="${1}"
-n_epochs="${2}"
-log_alpha="${3}"
-log_lambda="${4}"
+epoch="${2}"
+alpha="${3}"
+lambda="${4}"
 
-srun python PY-run_simulations.py "SEM" "${lr}" "${n_epochs}" "${log_alpha}" "${log_lambda}"
-srun python PY-run_simulations.py "LSTM" "${lr}" "${n_epochs}" "${log_alpha}" "${log_lambda}"
+srun python PY-run_simulations.py "SEM" "${lr}" "${epoch}" "${alpha}" "${lambda}"
+srun python PY-run_simulations.py "LSTM" "${lr}" "${epoch}" "${alpha}" "${lambda}"
 
 sacct --format="CPUTime,MaxRSS"
---
+++ loop_jobsub.cmd
@@ -0,0 +1,20 @@
+
+# parameter search over lr, n_epochs, alpha, lambda
+# declare -a lr_arr=(0.0001 0.0005 0.001 0.005 0.01 0.05 0.1)
+declare -a lr_arr=(0.01 0.05 0.1)
+# declare -a epoch_arr=(1 2 4 8 16 32 64)
+declare -a epoch_arr=(1 4 16 64)
+# declare -a alpha_arr=(-32 -16 -8 -4 -2 0 2 4 8 16 32)
+declare -a alpha_arr=(-32 -8 0 16 32)
+# declare -a lambda_arr=(-32 -16 -8 -4 -2 0 2 4 8 16 32)
+declare -a lambda_arr=(-32 -8 0 16 32)
+
+for lr in "${lr_arr[@]}"; do 
+  for epoch in "${epoch_arr[@]}"; do 
+    for alpha in "${alpha_arr[@]}"; do 
+      for lambda in "${lambda_arr[@]}"; do 
+        sbatch cpu_jobsub.cmd "${lr}" "${epoch}" "${alpha}" "${lambda}"
+      done
+    done
+  done
+done

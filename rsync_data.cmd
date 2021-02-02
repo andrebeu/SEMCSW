1975b2b436ac00b090f03c17b52003b07fc324ec
andrebeu <aobeukers@gmail.com>
2021-02-02 14:32:24 -0500

    no major updates

--- rsync_data.cmd
+++ rsync_data.cmd
@@ -1,1 +1,1 @@
-rsync -r -vam --update abeukers@scotty.princeton.edu:/jukebox/norman/abeukers/sem/SchemaPrediction_internal/data /Users/abeukers/wd/csw/SchemaPrediction_internal/data
+rsync -r -vam abeukers@scotty.princeton.edu:/jukebox/norman/abeukers/sem/SchemaPrediction_internal/data/* /Users/abeukers/wd/csw/SchemaPrediction_internal/data

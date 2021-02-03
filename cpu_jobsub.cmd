f09acef92606a45ee49ffb6019b408def4180b2b
andrebeu <aobeukers@gmail.com>
2021-02-03 08:00:09 -0500

    first branch commit

--- cpu_jobsub.cmd
+++ cpu_jobsub.cmd
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 #SBATCH -t 48:00:00   # runs for 48 hours (max)  
-#SBATCH --ntasks-per-node=1
 #SBATCH -N 1         # node count 
-#SBATCH -c 4         # number of cores 
+#SBATCH -c 1         # number of cores 
+#SBATCH --mem 3000
 #SBATCH -o ./slurms/output.%j.%a.out
 #SBATCH -e ./slurms/err.%j.%a
 
@@ -22,3 +22,4 @@ lambda="${5}"
 srun python PY-run_simulations.py "${model}" "${lr}" "${epoch}" "${alpha}" "${lambda}"
 
 sacct --format="CPUTime,MaxRSS"
+

77bd3ebe2159401ef00349e782806d9044ac8dc8
andrebeu <aobeukers@gmail.com>
2021-02-04 12:03:34 -0500

    minor bug

--- loop_jobsub.cmd
+++ loop_jobsub.cmd
@@ -4,7 +4,6 @@
 declare -a lr_arr=(0.001 0.005 0.01 0.05)
 declare -a epoch_arr=(1 2 4 8 16 32 64)
 declare -a cond_arr=("blocked" "interleaved")
-
 declare -a alfa_arr=(-32 -16 -8 -4 -2 0 2 4 8 16 32)
 declare -a lmbda_arr=(-32 -16 -8 -4 -2 0 2 4 8 16 32)
 
@@ -13,7 +12,7 @@ for seed in {0..19}; do
     for alfa in "${alfa_arr[@]}"; do
       for lmbda in "${lmbda_arr[@]}"; do 
         for epoch in "${epoch_arr[@]}"; do 
-          for cond in "${cond_arr[@]}"
+          for cond in "${cond_arr[@]}"; do
             sbatch cpu_jobsub.cmd "SEM" "${cond}" "${lr}" "${epoch}" "${alfa}" "${lmbda}" "${seed}"
             sbatch cpu_jobsub.cmd "LSTM" "${cond}" "${lr}" "${epoch}" "${alfa}" "${lmbda}" "${seed}"
           done

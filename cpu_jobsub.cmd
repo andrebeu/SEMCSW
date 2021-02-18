861cb48e5e06e032615666351c6fce28e99b7bf1
abeukers_on_scotty <aobeukers@gmail.com>
2021-02-18 12:10:51 -0500

    .

--- cpu_jobsub.cmd
+++ cpu_jobsub.cmd
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#SBATCH -t 24:00:00   # runs for 48 hours (max)  
+#SBATCH -t 4:00:00   # runs for 48 hours (max)  
 #SBATCH -N 1         # node count 
 #SBATCH -c 1         # number of cores 
 #SBATCH --mem 3000
